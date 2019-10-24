149e0cbc6bcb51259a0402a4df339b54dc333a08
totorototo <el.totorototo@gmail.com>
2019-10-24 14:19:49 +0200

    code clean-up

--- src/components/specific/app/App.js
+++ src/components/specific/app/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 import { actionCreators } from "../../../store";
 import {
@@ -25,49 +26,60 @@ const StyledAside = styled(Aside)`
   ${media.sm`display:none;`};
 `;
 
-const App = ({ requestBranch, ...rest }) => (
-  <Chrome
-    overflowX="scroll"
-    height="100vh"
-    width={[ 1 ]}
-    template={template}
-    header={area => (
-      <Header height={56} position="fixed" bg="pink" gridArea={area}>
-        <Box mr={4}>header</Box>
-      </Header>
-    )}
-    footer={area => (
-      <Footer bg="green" gridArea={area}>
-        <Box mr={4}>footer</Box>
-      </Footer>
-    )}
-    sidebar={area => (
-      <Sidebar alignItems={[ "flex-end", "center" ]} bg="blue" gridArea={area}>
-        <Box mr={[ 4, 0 ]}>sd</Box>
-      </Sidebar>
-    )}
-    main={area => (
-      <Main bg="brown" gridArea={area}>
-        {items.map(item => <Box>{item}</Box>)}
-      </Main>
-    )}
-    aside={area => (
-      <StyledAside bg="yellow" gridArea={area}>
-        <Flex
-          width={1}
-          bg="orange"
-          alignItems="flex-start"
-          justifyContent="flex-start"
-          flexDirection="column"
+const App = ({ tracklocation, setState, ...rest }) => {
+  return (
+    <Chrome
+      {...rest}
+      overflowX="scroll"
+      height="100vh"
+      width={[ 1 ]}
+      template={template}
+      header={area => (
+        <Header height={56} position="fixed" bg="pink" gridArea={area}>
+          <Box mr={4}>header</Box>
+        </Header>
+      )}
+      footer={area => (
+        <Footer bg="green" gridArea={area}>
+          <Box mr={4}>footer</Box>
+        </Footer>
+      )}
+      sidebar={area => (
+        <Sidebar
+          alignItems={[ "flex-end", "center" ]}
+          bg="blue"
+          gridArea={area}
         >
-          <Box>toto</Box>
-          <Box>titi</Box>
-        </Flex>
-      </StyledAside>
-    )}
-  />
-);
+          <Box mr={[ 4, 0 ]}>sd</Box>
+        </Sidebar>
+      )}
+      main={area => (
+        <Main bg="brown" gridArea={area}>
+          <button
+            onClick={() => {
+              setState({ tracklocation: !tracklocation });
+            }}
+          >
+            toto
+          </button>
+        </Main>
+      )}
+      aside={area => (
+        <StyledAside bg="yellow" gridArea={area}>
+          <Flex
+            width={1}
+            bg="orange"
+            alignItems="flex-start"
+            justifyContent="flex-start"
+            flexDirection="column"
+          >
+            <Box>toto</Box>
+            <Box>titi</Box>
+          </Flex>
+        </StyledAside>
+      )}
+    />
+  );
+};
 
-export default connect(null, {
-  requestBranch: actionCreators.receiveBranch
-})(App);
+export default App;
