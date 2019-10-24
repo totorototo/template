8a4b96b4ef38e83fe048be69e10868228dfe7cd6
totorototo <el.totorototo@gmail.com>
2019-10-24 14:20:27 +0200

    update aperture function, add location stream

--- src/index.js
+++ src/index.js
@@ -1,91 +1,87 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
-import { Provider } from "react-redux";
 import { withEffects } from "refract-callbag";
 import fromPromise from "callbag-from-promise";
-import { filter, map, merge, combine, pipe, flatten } from "callbag-basics";
+import { map, pipe, fromEvent, flatten, combine } from "callbag-basics";
+import CbOf from "callbag-of";
+import interval from "callbag-interval";
+import startWith from "callbag-start-with";
+import dropRepeats from "callbag-drop-repeats";
 
 import { App } from "./components";
 import { theme } from "./theme/theme";
-import { actionCreators, actionTypes, selectors } from "./store";
-import store from "./setupStore";
+import { location } from "./helpers/location";
+import StateContainer from "./StateContainer";
 
 const GlobalStyle = createGlobalStyle`
   body {    
-    font-family: "gilbert";
+    font-family: "roboto";
     font-size:2em;
     margin: 0px;
   }
 `;
 
 const apiDependency = {
-  getBranch: name =>
-    fromPromise(
-      fetch(`https://api.github.com/users/${name}`)
-        .then(response => response.json())
-        .catch(error => ({ error }))
-    )
+  getCurrentlocation: settings => fromPromise(location(settings))
 };
 
-const aperture = (component, { store, api }) => {
-  const combined$ = combine(
-    store.observe(actionTypes.BRANCH_REQUEST),
-    store.observe(selectors.getBranches)
+const toState = payload => ({ type: "SET_STATE", payload });
+const isVisible = () => document.visibilityState === "visible";
+
+const aperture = (component, { api }) => {
+  const tracklocation$ = component.observe("tracklocation");
+
+  const visible$ = pipe(
+    fromEvent.default(document, "visibilitychange"),
+    map(isVisible),
+    startWith(isVisible())
   );
 
-  const requestBranch$ = pipe(
-    combined$,
-    filter(([ request, branches ]) => !Boolean(branches[request.payload])),
-    map(([ { payload: name } ]) => {
-      debugger;
-      return api.getBranch(name);
-    }),
-    flatten.default,
+  const track$ = pipe(
+    combine(visible$, tracklocation$),
+    map(([ visible, tracked ]) => visible && tracked),
+    dropRepeats(),
     map(
-      ({ message, ...response }) =>
-        Boolean(message)
-          ? actionCreators.receiveError(message)
-          : actionCreators.receiveBranch(response)
-    )
+      visibleAndTracked =>
+        visibleAndTracked
+          ? pipe(
+              interval(1000),
+              map(() => api.getCurrentlocation()),
+              flatten.default,
+              map(({ coords }) => ({ coords })),
+              map(toState)
+            )
+          : CbOf({ type: "Oops" })
+    ),
+    flatten.default
   );
 
-  const selectBranch$ = pipe(
-    combined$,
-    filter(([ request, branches ]) => Boolean(branches[request.payload])),
-    map(([ { payload } ]) => payload),
-    map(actionCreators.selectBranch)
-  );
-
-  return merge(requestBranch$, selectBranch$);
+  return track$;
 };
 
-const handler = ({ store }) => effect => {
+const handler = ({ setState }) => effect => {
   switch (effect.type) {
-    case actionTypes.ERROR_RECEIVE:
-      return console.log(effect);
-
-    case actionTypes.BRANCH_RECEIVE:
-      return store.dispatch(effect);
-
-    case actionTypes.BRANCH_SELECT:
-      return store.dispatch(effect);
-
+    case "SET_STATE":
+      console.log(effect.payload);
+      return setState(effect.payload);
     default:
       return;
   }
 };
 
-const LayoutWithEffects = withEffects(aperture, { handler })(App);
+const errorHandler = () => err => console.error(err);
+
+const LayoutWithEffects = withEffects(aperture, { handler, errorHandler })(App);
 
 const Application = () => {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <LayoutWithEffects store={store} api={apiDependency} />
-        <GlobalStyle />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <StateContainer>
+        {state => <LayoutWithEffects {...state} api={apiDependency} />}
+      </StateContainer>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 };
 
